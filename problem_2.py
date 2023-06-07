47dca63b2c364867e78d06e59d7889f72a3586b2
Matthew Cronin <matthew.j.cronin@gmail.com>
2023-06-06 21:08:29 -0500

    Use consistent filenames.

---
+++ problem_2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import argparse
+
+
+def solution() -> None:
+    """ """
+
+    return
+
+
+def main():
+    """Project Euluer problem """
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    problem_statement = """
+
+    """.format()
+    print(problem_statement)
+
+    answer = solution()
+
+    print("""
+        Answer: {}
+        """.format(answer)
+    )
+
+
+if __name__ == '__main__':
+    main()
